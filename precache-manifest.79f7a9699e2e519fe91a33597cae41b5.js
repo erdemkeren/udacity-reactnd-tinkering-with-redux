cbb45132566253118c0f0b5277aeed0e829d39bb
Hilmi Erdem KEREN <erdem.keren@gmail.com>
2019-07-24 12:47:11 +0300

    Updates

---
+++ precache-manifest.79f7a9699e2e519fe91a33597cae41b5.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb1e84c4aa4904f6b805eb90e2cda51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50b7de3fa202dfff4002",
+    "url": "/static/js/main.2671805a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
